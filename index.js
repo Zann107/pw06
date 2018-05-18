832f365832c765ad96c58a8f3e56478ca1af7ff7
Scott <ameganscott@gmail.com>
2018-05-18 17:44:35 +0200

    un comptdown basic

--- index.js
+++ index.js
@@ -1,31 +1,34 @@
-console.dir(document);
+
+function timevent(){
+
+var datenow = new Date();
+var datevent = new Date("May 25 15:30:00 2018");
+var timer = (datevent - datenow) / 1000;
+var jours = Math.floor(timer / (60 * 60 * 24));
+var heures = Math.floor((timer - (jours * 60 * 60 * 24)) / (60 * 60));
+var minutes = Math.floor((timer - ((jours * 60 * 60 * 24 + heures * 60 * 60))) / 60);
+var secondes = Math.floor(timer - ((jours * 60 * 60 * 24 + heures * 60 * 60 + minutes * 60)));
+timer = Math.abs(timer);
 
 
+var day= document.getElementById("day");
 
+day.innerHTML = jours;
 
+var hour = document.getElementById("hour");
 
+hour.innerHTML = heures;
 
-var compteur=document.getElementById('chronotime');
+var min = document.getElementById("min");
 
+min.innerHTML = minutes;
 
-                  
+var second = document.getElementById("second");
 
+second.innerHTML = secondes;
 
-function timer(){
-    var chrono = setInterval(tmin(),60000);
+var actualisation = setTimeout("timevent();", 1000);
+
 }
-
-
-
-
-var min = document.getElementById('minutes').value;
-
-function tmin(){
-    if (min > 0) {
-        min--;
-    }
-    if (min == 0) {
-        var fin = setInterval(alert("Fin du break"),60000);
-    }
-}
-chronotime.innerHTML= min +':';
+timevent();
+console.log(document);
